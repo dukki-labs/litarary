09a874234ab14f0025a469f88b835223261695ba
Lee June Hee <39672033+phantom08266@users.noreply.github.com>
2023-01-19 22:47:38 +0900

    [Feature/2] Postgres Database 연동  (#5)
    * Feat: [#2] DB연동 및 상태체크 컨트롤러 추가
    
    * Feat: [#2] DB연결 테스트를 위한 CD 트리거 임시 변경
    
    * Feat: [#2] 디비연결 테스트 확인하기 위해 빌드 시 테스트 코드 무시하도록 설정
    
    * Feat: [#2] Postgres로 디비 변경
    
    * Feat: [#2] Postgres로 디비 변경
    
    * Feat: [#2] 신규 Postgres DB로 계정 변경
    
    * Feat: [#2] Dockerfile에 DB 환경변수 설정값 추가
    
    * Feat: [#2] fly.io가 환경변수를 넣어줌에 따라 Dockerfile에 설정한 환경변수 제거
    
    * Feat: [#2] backend 하위 디렉토리가 변경되면 배포되도록 수정
    
    * Feat: [#2] 오타들어간 부분 제거

---
+++ backend/src/main/java/com/litarary/entity/Health.java
@@ -0,0 +1,23 @@
+package com.litarary.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Health {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String status;
+    private int code;
+
+    public void saveHealth(String status, int code) {
+        this.status = status;
+        this.code = code;
+    }
+}
