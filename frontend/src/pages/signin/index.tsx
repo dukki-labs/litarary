be87c6a0c3e04ff366c9f12c8d51d64f249bad44
ted-han <taekyung28h@gmail.com>
2023-02-09 20:52:39 +0900

    Feat: [#16] 로그인 / 회원가입 디자인바뀐부분 적용

--- frontend/src/pages/signin/index.tsx
+++ frontend/src/pages/signin/index.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
+import Image from "next/image";
 import Text from "@/components/common/Text";
 import styles from "@/styles/Entrance.module.scss";
+import icon_eye_on from "@/img/icon_eye_on.svg";
+import icon_eye_off from "@/img/icon_eye_off.svg";
 
 export default function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [active, setActive] = useState(0);
+  const [isEyeOn, setIsEyeOn] = useState(false);
   const router = useRouter();
 
   return (
@@ -25,18 +30,28 @@ export default function SignIn() {
             </span>
           </div>
           <div className={styles.singinBox}>
-            <div className={styles.inputBox}>
+            <div className={`${styles.inputBox} ${active === 1 && styles.on}`}>
               <input
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 placeholder="이메일"
+                onFocus={() => setActive(1)}
+                onBlur={() => setActive(0)}
               />
             </div>
-            <div className={styles.inputBox}>
+            <div className={`${styles.inputBox} ${active === 2 && styles.on}`}>
               <input
                 value={password}
+                type={isEyeOn ? "text" : "password"}
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="패스워드"
+                onFocus={() => setActive(2)}
+                onBlur={() => setActive(0)}
+              />
+              <Image
+                src={isEyeOn ? icon_eye_on : icon_eye_off}
+                alt=""
+                onClick={() => setIsEyeOn(!isEyeOn)}
               />
             </div>
             <div
@@ -44,13 +59,26 @@ export default function SignIn() {
                 email && password ? styles.on : ""
               }`}
             >
-              <Text text="로그인" fontSize={24} lineHeight={32} color="white" />
+              <Text
+                text="로그인하기"
+                fontSize={24}
+                lineHeight={32}
+                color="white"
+              />
             </div>
-            <div className={styles.etc}>
-              <div onClick={() => router.push("/signup")}>
-                <Text text="회원가입" fontSize={20} lineHeight={28} />
-              </div>
-              <Text text="패스워드 찾기" fontSize={20} lineHeight={28} />
+            <div className={styles.lost}>
+              <Text
+                text="이런, 계정을 분실했나요?"
+                fontSize={16}
+                lineHeight={24}
+                color="#1A1A1A"
+              />
+            </div>
+            <div
+              className={styles.signupButton}
+              onClick={() => router.push("/signup")}
+            >
+              <span>리터러리 가입하기</span>
             </div>
           </div>
         </div>
--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
 import Head from "next/head";
-import Text from "@/components/common/Text";
 import styles from "@/styles/Entrance.module.scss";
+import Title from "@/components/signup/Title";
+import Terms from "@/components/signup/Terms";
+import Interest from "@/components/signup/Interest";
 
-export default function SignIn() {
-  const [terms, setTerms] = useState<boolean[]>([false, false, false, false]);
+export default function SignUp() {
+  const [step, setStep] = useState<number>(1);
+  const [terms, setTerms] = useState<boolean[]>([false, false, false]);
 
   const onClickTerm = (idx: number) => {
-    if (idx === 4) {
+    if (idx === 3) {
       const agreeCnt = terms.filter((v) => v).length;
-      if (agreeCnt === 4) {
-        setTerms([false, false, false, false]);
+      if (agreeCnt === 3) {
+        setTerms([false, false, false]);
         return;
       } else {
-        setTerms([true, true, true, true]);
+        setTerms([true, true, true]);
         return;
       }
     }
@@ -27,6 +30,13 @@ export default function SignIn() {
     });
   };
 
+  const onClickButton = () => {
+    if (step === 1 && terms[0] && terms[1]) {
+      setStep(3);
+      return;
+    }
+  };
+
   return (
     <>
       <Head>
@@ -37,114 +47,20 @@ export default function SignIn() {
         <section>
           <div className={styles.bar}>
             <div className={styles.progress}></div>
-            <div className={styles.step}></div>
+            {step === 1 && <div className={styles.step} />}
+            {step === 2 && <div className={`${styles.step} ${styles.two}`} />}
+            {step === 3 && <div className={`${styles.step} ${styles.three}`} />}
           </div>
-          <div className={styles.title}>
-            <span>원활한 이용을 위해서</span>
-            <br />
-            <span>
-              <span className={styles.bold}>리터러리 약관</span>에 동의해주세요.
-            </span>
-          </div>
-          <div className={styles.signupWrapper}>
-            <div className={styles.allAgree}>
-              <div
-                className={`${styles.check} ${
-                  terms[0] && terms[1] && terms[2] && terms[3] ? styles.on : ""
-                }`}
-                onClick={() => onClickTerm(4)}
-              />
-              <Text
-                text="전체 동의 (선택 포함)"
-                fontSize={20}
-                lineHeight={28}
-              />
-            </div>
-            <div className={styles.termsBox}>
-              <div className={styles.terms}>
-                <div
-                  className={`${styles.check} ${terms[0] ? styles.on : ""}`}
-                  onClick={() => onClickTerm(0)}
-                />
-                <Text
-                  text="(필수) 리터러리 계정 약관 동의"
-                  fontSize={20}
-                  lineHeight={28}
-                />
-              </div>
-              <div className={styles.termsDesc}>
-                <Text
-                  text="계정 약관 동의 텍스트"
-                  fontWeight={400}
-                  fontSize={12}
-                  lineHeight={16}
-                />
-              </div>
-            </div>
-            <div className={styles.termsBox}>
-              <div className={styles.terms}>
-                <div
-                  className={`${styles.check} ${terms[1] ? styles.on : ""}`}
-                  onClick={() => onClickTerm(1)}
-                />
-                <Text
-                  text="(필수) 리터러리 서비스 약관 동의"
-                  fontSize={20}
-                  lineHeight={28}
-                />
-              </div>
-              <div className={styles.termsDesc}>
-                <Text
-                  text="서비스 약관 동의 텍스트"
-                  fontWeight={400}
-                  fontSize={12}
-                  lineHeight={16}
-                />
-              </div>
-            </div>
-            <div className={styles.termsBox}>
-              <div className={styles.terms}>
-                <div
-                  className={`${styles.check} ${terms[2] ? styles.on : ""}`}
-                  onClick={() => onClickTerm(2)}
-                />
-                <Text
-                  text="(필수) 개인정보 수집 및 이용 안내"
-                  fontSize={20}
-                  lineHeight={28}
-                />
-              </div>
-              <div className={styles.termsDesc}>
-                <Text
-                  text="개인정보 수집 및 이용 안내 텍스트"
-                  fontWeight={400}
-                  fontSize={12}
-                  lineHeight={16}
-                />
-              </div>
-            </div>
-            <div className={styles.termsBox}>
-              <div className={styles.terms}>
-                <div
-                  className={`${styles.check} ${terms[3] ? styles.on : ""}`}
-                  onClick={() => onClickTerm(3)}
-                />
-                <Text
-                  text="(선택) 서비스 안내 수신 동의"
-                  fontSize={20}
-                  lineHeight={28}
-                />
-              </div>
-            </div>
-            <div
-              className={`${styles.button} ${
-                terms[0] && terms[1] && terms[2] ? styles.on : ""
-              }`}
-            >
-              <Text text="다음" fontSize={24} lineHeight={32} color="white" />
-            </div>
-          </div>
+          <Title step={step} />
+          {step === 1 && (
+            <Terms
+              terms={terms}
+              onClickTerm={onClickTerm}
+              onClickButton={onClickButton}
+            />
+          )}
         </section>
+        {step === 3 && <Interest onClickButton={onClickButton} />}
       </main>
     </>
   );
