563dc0a6704a22683e64967b3ebb2b99a0c9fb48
phantom08266 <phantom08266@gmail.com>
2023-04-22 16:33:34 +0900

    Feat: [#98] 추천 도서 카테고리 목록 조회 조건 추가
    - 카테고리에 등록된 도서가 1개이상 있을 경우의 카테고리만 조회되도록 수정

--- src/main/java/com/litarary/book/repository/CategoryRepository.java
+++ src/main/java/com/litarary/book/repository/CategoryRepository.java
@@ -14,6 +14,14 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByBookCategory(BookCategory bookCategory);
     List<Category> findByBookCategoryIn(List<BookCategory> bookCategories);
 
-    @Query("select c from Category c inner join MemberCategory mc on c.id = mc.category.id where mc.member = :member")
+    @Query("select c " +
+            "from Category c " +
+            "inner join MemberCategory mc " +
+            "on c.id = mc.category.id " +
+            "inner join Book b " +
+            "on b.category.id = c.id " +
+            "where mc.member = :member " +
+            "group by c.id " +
+            "having count(b.id) > 0")
     List<Category> findCategoriesByMember(@Param("member") Member member);
 }
